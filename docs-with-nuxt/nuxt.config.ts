b8d965975d9e7bd4b59159f52ec2ec9392070217
kieranwv <kieranwme@gmail.com>
2025-07-29 17:56:23 +0800

    feat: add docs with nuxt

---
+++ docs-with-nuxt/nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@element-plus/nuxt',
+  ],
+  elementPlus: {
+    /** Options */
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
+  app: {
+    head: {
+      title: 'Linqet · 灵雀快搭 - AI 驱动的全场景可视化平台',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '由 AI 驱动，专为营销人员、中小商户、独立开发者打造的全场景可视化平台，快速创建活动页面、海报、H5 专区、微商城等场景化应用' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
+})
